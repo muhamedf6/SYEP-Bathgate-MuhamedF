a7dbdaa9d0d87e81e63f4626e7477c5e2a568d51
muhamedf6 <muhamedfisi60@gmail.com>
2025-08-05 15:51:07 +0000

    Added testimonials and more styling

--- script.js
+++ script.js
@@ -1,3 +1,77 @@
+const reviews = [
+{
+    id: 1,
+    name: 'Susan Smith',
+    job: 'Web Developer',
+    img: '#',
+    text: 'Lorem ipsum'
+},
+
+{
+    id: 2,
+    name: 'Anna Johnson',
+    job: 'Web Designer'
+    img: '#',
+    text: 'Lorem ipsum'
+},
+
+{
+    id: 3,
+    name: 'Peter Jones',
+    job: 'Intern',
+    img: '#',
+    text:
+}
+],
+
+//select items
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+
+let currentItem = 0;
+
+window.addEventListener('DOMContentLoaded', function(){
+    const item = reviews[currentItem];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+     info.textContent = item.text;
+});
+
+function showPerson(person){
+    const item = reviews[person];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+     info.textContent = item.text;
+    }
+
+nextBtn.addEventListener('click', function (){
+    currentItem++;
+    if (currentItem > reviews.length - 1) {
+        currentItem = 0;
+    }
+    showPerson(currentItem);
+});
+
+prevBtn.addEventListener('click', function (){
+    currentItem--;
+    if (currentItem < 0) {
+        currentItem = reviews.length - 1;
+    }
+    showPerson(currentItem);
+});
+
+
+
+
+
+
 console.log("Hello World!");
 
 let myString = "a2b3c";
